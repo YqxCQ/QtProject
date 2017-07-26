73de6588ff77b548b8ca5f70a1bfb740a4777c8f
YqxCQ <YqxChenqi@163.com>
2017-07-26 15:42:55 +0800

    V1.1.2 This is third verson

--- GitTest/main.cpp
+++ GitTest/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     //w.show();
-    w.showMaximized();
+    //w.showMaximized();
 
-
+    w.showFullScreen();
     return a.exec();
 }
